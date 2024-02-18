da3fd9c0b3f1ad229cafc31ace273bc083ef8eba
LetsGoAway <ethpsa09@gmail.com>
2024-02-18 21:13:48 +0800

    add wsad controls to sonic cd (will make a controls system eventually for now simple fix)

--- iframe_inject.js
+++ iframe_inject.js
@@ -110,7 +110,67 @@ else {
                 f.readAsDataURL(bb);
             }
         }, 1000);
+
+
+        // Keybinding fix
+        Window.prototype.addEventListener = ((origFn) => {
+            return function (type, callback, options) {
+                if (type == "keydown" || type == "keyup" || type == "keypress") {
+                    callback = ((origCall) => {
+                        return function (ev) {
+                            if (ev.keyCode == 87) {
+                                ev = new KeyboardEvent(type, { // W -> Forward
+                                    keyCode: 38,
+                                    which: 38,
+                                    timestep: ev.timestep,
+                                });
+                            }
+                            else if (ev.keyCode == 83) { // S -> down
+                                ev = new KeyboardEvent(type, {
+                                    keyCode: 40,
+                                    which: 40,
+                                    timestep: ev.timestep,
+                                });
+                            }
+                            else if (ev.keyCode == 65) {
+                                ev = new KeyboardEvent(type, { // A -> Left
+                                    keyCode: 37,
+                                    which: 37,
+                                    timestep: ev.timestep,
+                                });
+                            }
+                            else if (ev.keyCode == 68) {
+                                ev = new KeyboardEvent(type, { // D -> Right
+                                    keyCode: 39,
+                                    which: 39,
+                                    timestep: ev.timestep,
+                                });
+                            }
+                            else if (ev.keyCode == 27) {
+                                ev = new KeyboardEvent(type, { // ESC -> X
+                                    keyCode: 88,
+                                    which: 88,
+                                    timestep: ev.timestep,
+                                });
+                            }
+                            else if (ev.keyCode == 32) {
+                                ev = new KeyboardEvent(type, { // Space -> z
+                                    keyCode: 90,
+                                    which: 90,
+                                    timestep: ev.timestep,
+                                });
+                            }
+                            //console.log(options);
+                            return origCall.call(this, ev);
+                        };
+                    })(callback);
+                }
+                //console.log(options);
+                return origFn.call(this, type, callback, options);
+            };
+        })(Window.prototype.addEventListener);
     }
+
     function raf(timestep) {
         Object.keys(window.localStorage).forEach((key) => {
             if (window.localStorage.getItem(key) != null && key != 'gat_settings') {
