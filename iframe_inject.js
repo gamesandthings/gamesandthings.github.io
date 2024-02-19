b53727c374a644e1bbe6ca3594bf82473486e08a
LetsGoAway <ethpsa09@gmail.com>
2024-02-19 20:06:00 +0800

    add wasd to subway surfers

--- iframe_inject.js
+++ iframe_inject.js
@@ -170,6 +170,50 @@ else {
             };
         })(Window.prototype.addEventListener);
     }
+    else if ("gameData" in window.top && window.top.gameData.title == "Subway Surfers"){
+        Window.prototype.addEventListener = ((origFn) => {
+            return function (type, callback, options) {
+                if (type == "keydown" || type == "keyup" || type == "keypress") {
+                    callback = ((origCall) => {
+                        return function (ev) {
+                            if (ev.keyCode == 87) {
+                                ev = new KeyboardEvent(type, { // W -> Forward
+                                    keyCode: 38,
+                                    which: 38,
+                                    timestep: ev.timestep,
+                                });
+                            }
+                            else if (ev.keyCode == 83) { // S -> down
+                                ev = new KeyboardEvent(type, {
+                                    keyCode: 40,
+                                    which: 40,
+                                    timestep: ev.timestep,
+                                });
+                            }
+                            else if (ev.keyCode == 65) {
+                                ev = new KeyboardEvent(type, { // A -> Left
+                                    keyCode: 37,
+                                    which: 37,
+                                    timestep: ev.timestep,
+                                });
+                            }
+                            else if (ev.keyCode == 68) {
+                                ev = new KeyboardEvent(type, { // D -> Right
+                                    keyCode: 39,
+                                    which: 39,
+                                    timestep: ev.timestep,
+                                });
+                            }
+                            //console.log(options);
+                            return origCall.call(this, ev);
+                        };
+                    })(callback);
+                }
+                //console.log(options);
+                return origFn.call(this, type, callback, options);
+            };
+        })(Window.prototype.addEventListener);
+    }
 
     function raf(timestep) {
         Object.keys(window.localStorage).forEach((key) => {
