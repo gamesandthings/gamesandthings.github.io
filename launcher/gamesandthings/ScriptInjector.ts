cc99b71c33af49156c20d9807196fe3366757aa6
LetsGoAway <68365423+letsgoawaydev@users.noreply.github.com>
2025-03-12 21:51:38 +0800

    fix script injection

--- launcher/gamesandthings/ScriptInjector.ts
+++ launcher/gamesandthings/ScriptInjector.ts
@@ -1,18 +1,13 @@
 import Launcher from "../Launcher";
 export default class ScriptInjector {
-
-    public static injectMap = new Map<String,Boolean>();
-
     public static inject(id: string) {
         if (Launcher.iframe.contentWindow == null) return;
-        if (this.injectMap.has(id)) return;
         let s = Launcher.iframe.contentDocument?.createElement('script');
         if (s != undefined) {
             s.type = 'text/javascript';
             s.src = id;
             Launcher.iframe.contentDocument?.head.prepend(s);
             console.log("[ScriptInjector] Injected " + id);
-            this.injectMap.set(id, true);
         }
     }
 }
